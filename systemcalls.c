33022fcb3f4fbf3eef9977c1d23d433005e1646c
BerinaCrncalo <berina.crncalo@stu.ibu.edu.ba>
2024-03-26 23:32:53 +0100

    UPDATE of forkbomb to prevent crashes

--- systemcalls.c
+++ systemcalls.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <signal.h>
+
+#define MAX_CHILDREN 1000 // Maximum number of child processes
 
 void execute_ls() {
     // Execute the 'ls -l' command
@@ -13,11 +16,33 @@ void execute_ls() {
 }
 
 void fork_bomb() {
+    int child_count = 0;
+
     while (1) {
-        fork(); // Fork indefinitely
+        pid_t pid = fork(); // Fork a child process
+
+        if (pid < 0) {
+            fprintf(stderr, "Fork failed\n");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+            printf("Child process (PID %d) is starting...\n", getpid());
+            // Execute any desired operation in the child process
+            // For example, you can uncomment the line below to execute 'ls -l'
+            // execute_ls();
+            exit(EXIT_SUCCESS);
+        } else {
+            // Parent process
+            child_count++;
+            if (child_count >= MAX_CHILDREN) {
+                // Terminate some child processes to prevent resource exhaustion
+                // You can use various strategies here, such as killing the oldest children or randomly selecting children to terminate
+                // For simplicity, let's exit the child process immediately
+                exit(EXIT_SUCCESS);
+            }
+        }
     }
 }
-
 int main() {
     pid_t pid;
     
